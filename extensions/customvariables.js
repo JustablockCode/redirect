be4639ac13acee0d75b0d1b2378120334bf099f8
JustablockCode <bdfd000@gmail.com>
2024-02-10 20:39:27 +0100

    Create customvariables.js

---
+++ extensions/customvariables.js
@@ -0,0 +1,93 @@
+// MADE BY JUSTABLOCK FOR PENGUINMOD
+class CustomVar {
+    constructor() {
+        this.variables = {};
+    }
+
+    getInfo() {
+        return {
+            id: "customvar",
+            name: "Custom Variables",
+            color1: "#00941e",
+            color2: "#00661a",
+            color3: "#012e00",
+            blocks:[
+                {
+                    opcode: "varset",
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: "Set variable [VAR] to [VALUE]",
+                    arguments: {
+                        VAR: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "hello"
+                        },
+                        VALUE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "Hello, world!"
+                        }
+                    }
+                },
+                {
+                    opcode: "varget",
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: "Get variable [VAR]",
+                    arguments: {
+                        VAR: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                },
+                {
+                    opcode: "varexists",
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: "Variable [VAR] exists?",
+                    arguments: {
+                        VAR: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                },
+                {
+                    opcode: "vardelete",
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: "Delete variable [VAR]",
+                    arguments: {
+                        VAR: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                },
+            ]
+        }
+    }
+
+    varset(args) {
+        if (typeof args.VAR !== 'undefined') {
+            this.variables[args.VAR] = args.VALUE;
+        } else {
+            console.error('Variable name is not defined');
+        }
+    }
+
+    varget(args) {
+        if (typeof args.VAR !== 'undefined') {
+            return this.variables[args.VAR] || 'undefined';
+        } else {
+            console.error('Variable name is not defined');
+            return 'undefined';
+        }
+    }
+
+    varexists(args) {
+        return typeof this.variables[args.VAR] !== 'undefined';
+    }
+
+    vardelete(args) {
+        delete this.variables[args.VAR];
+    }
+}
+
+Scratch.extensions.register(new CustomVar());
